c9cb4a1c9d4196edba6db22b08df597977fa8f6e
Nichanan Tantiwatanapaisal <s61122202074@ssru.ac.th>
2021-07-14 17:01:48 +0700

    สำหรับตรวจ รอบที่ 1 ค่ะ

--- app/src/main/java/com/example/covid_19/adapter/Adapter.kt
+++ app/src/main/java/com/example/covid_19/adapter/Adapter.kt
@@ -4,35 +4,88 @@ import android.content.Context
 import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Filter
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.covid_19.R
 import com.example.covid_19.models.Data
 import com.example.covid_19.ui.DetailActivity
 import com.squareup.picasso.Picasso
+import java.text.DecimalFormat
+import java.text.NumberFormat
+import java.util.*
+import kotlin.collections.ArrayList
 
-class Adapter (private val postList: List<Data>, private val context: Context) : RecyclerView.Adapter<ViewHolder>() {
+
+class Adapter (private var postList: List<Data>, private val context: Context) : RecyclerView.Adapter<ViewHolder>() {
+    var txtNewConfirmed: TextView? = null
+    var txtTotalConfirmed: TextView? = null
+    var txtNewDeaths: TextView? = null
+    var txtTotalDeaths: TextView? = null
+    var txtNewRecovered: TextView? = null
+    var txtTotalRecovered: TextView? = null
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        return ViewHolder(LayoutInflater.from(context).inflate(R.layout.country_item, parent, false))
+        return ViewHolder(
+            LayoutInflater.from(context).inflate(R.layout.country_item, parent, false)
+        )
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+
         holder.BindTextValue!!.text = postList[position].country
         holder.BindContentValue!!.text = postList[position].countryCode
         holder.BindCardView!!.setOnClickListener {
             val intent = Intent(context, DetailActivity::class.java)
             intent.putExtra("code", postList[position].countryCode)
             intent.putExtra("name", postList[position].country)
-            intent.putExtra("newConfirmed", postList[position].newConfirmed.toString())
-            intent.putExtra("totalConfirmed", postList[position].totalConfirmed.toString())
-            intent.putExtra("newDeaths", postList[position].newDeaths.toString())
-            intent.putExtra("totalDeaths", postList[position].totalDeaths.toString())
-            intent.putExtra("newRecovered", postList[position].newRecovered.toString())
-            intent.putExtra("totalRecovered", postList[position].totalRecovered.toString())
+
+            val newCfString: String = postList[position].newConfirmed.toString()
+            val totalCfString: String = postList[position].totalConfirmed.toString()
+            val newDtString: String = postList[position].newDeaths.toString()
+            val totalDtString: String = postList[position].totalDeaths.toString()
+            val newReString: String = postList[position].newRecovered.toString()
+            val totalReString: String = postList[position].totalRecovered.toString()
+            val longval: Long
+
+
+//            if ((newCfString).contains(",")) {
+//                newCfString = newCfString.replace(",".toRegex(), "")
+//            }
+//            longval = newCfString.toLong()
+
+//            val decim = DecimalFormat("#,###.##")
+//            newCfString.setText(decim.format(newConfirmed))
+//
+//            newCfString.setText(java.lang.String.format(Locale.getDefault(), "%,d", newConfirmed))
+
+//            NumberFormat.getInstance().format(newCfString)
+            intent.putExtra("newConfirmed", newCfString)
+            intent.putExtra("totalConfirmed", totalCfString)
+            intent.putExtra("newDeaths", newDtString)
+            intent.putExtra("totalDeaths", totalDtString)
+            intent.putExtra("newRecovered", newReString)
+            intent.putExtra("totalRecovered", totalReString)
+
+//            val formatter: DecimalFormat = NumberFormat.getInstance(Locale.US) as DecimalFormat
+//            formatter.applyPattern("#,###,###,###")
+//            val formattedString: String = formatter.format(longval)
+
+
+//                intent.putExtra("newConfirmed", postList[position].newConfirmed.toString())
+//            intent.putExtra("totalConfirmed", postList[position].totalConfirmed.toString())
+//            intent.putExtra("newDeaths", postList[position].newDeaths.toString())
+//            intent.putExtra("totalDeaths", postList[position].totalDeaths.toString())
+//            intent.putExtra("newRecovered", postList[position].newRecovered.toString())
+//            intent.putExtra("totalRecovered", postList[position].totalRecovered.toString())
+
             intent.putExtra("date", postList[position].date)
             intent.putExtra("time", postList[position].date)
             context.startActivity(intent)
         }
-        Picasso.get().load(postList[position].date).error(R.drawable.ic_baseline_arrow_forward_ios_24)
+        Picasso.get().load(postList[position].date)
+            .error(R.drawable.ic_baseline_arrow_forward_ios_24)
             .placeholder(R.drawable.ic_baseline_arrow_forward_ios_24)
             .into(holder.BindImageView)
     }
@@ -40,5 +93,141 @@ class Adapter (private val postList: List<Data>, private val context: Context) :
     override fun getItemCount(): Int {
         return postList.size
     }
+
+    fun getFilter(): Filter {
+        return object : Filter() {
+            override fun performFiltering(charSequence: CharSequence): FilterResults {
+                val charString = charSequence.toString()
+                val mArrayList = postList
+                if (charString.isEmpty()) {
+                    postList = mArrayList
+                } else {
+                    val filteredList: ArrayList<Data> = ArrayList()
+                    val mArrayList: ArrayList<Data>
+                    for (country in postList) {
+                        if (country.toString().lowercase(Locale.getDefault())
+                                .contains(charString) || country.toString()
+                                .lowercase(Locale.getDefault())
+//                                .contains(charString) || countryCode.toS.toLowerCase()
+                                .contains(charString)
+                        ) {
+                            filteredList.add(country)
+                        }
+                    }
+                    postList = filteredList
+                }
+                val filterResults = FilterResults()
+                filterResults.values = postList
+                return filterResults
+            }
+
+            override fun publishResults(charSequence: CharSequence, filterResults: FilterResults) {
+                postList = filterResults.values as ArrayList<Data>
+                notifyDataSetChanged()
+            }
+        }
+    }
+
+
+
+
+//    fun afterTextChanged(s: TextView) {
+//        try {
+//            txtNewConfirmed?.removeTextChangedListener(this)
+//            val value: String = txtNewConfirmed?.getText().toString()
+//            if (value != null && value != "") {
+//                if (value.startsWith(".")) {
+//                    txtNewConfirmed?.setText("0.")
+//                }
+//                if (value.startsWith("0") && !value.startsWith("0.")) {
+//                    txtNewConfirmed?.setText("")
+//                }
+//                val str: String = txtNewConfirmed?.getText().toString().replaceAll(",", "")
+//                if (value != "") txtNewConfirmed?.setText(getDecimalFormattedString(str))
+//                txtNewConfirmed.setSelection(txtNewConfirmed?.getText().toString().length)
+//            }
+//            txtNewConfirmed?.addTextChangedListener(this)
+//            return
+//        } catch (ex: Exception) {
+//            ex.printStackTrace()
+//            txtNewConfirmed?.addTextChangedListener(this)
+//        }
+//    }
+//
+//    fun getDecimalFormattedString(value: String): String? {
+//        val lst = StringTokenizer(value, ".")
+//        var str1 = value
+//        var str2 = ""
+//        if (lst.countTokens() > 1) {
+//            str1 = lst.nextToken()
+//            str2 = lst.nextToken()
+//        }
+//        var str3 = ""
+//        var i = 0
+//        var j = -1 + str1.length
+//        if (str1[-1 + str1.length] == '.') {
+//            j--
+//            str3 = "."
+//        }
+//        var k = j
+//        while (true) {
+//            if (k < 0) {
+//                if (str2.length > 0) str3 = "$str3.$str2"
+//                return str3
+//            }
+//            if (i == 3) {
+//                str3 = ",$str3"
+//                i = 0
+//            }
+//            str3 = str1[k].toString() + str3
+//            i++
+//            k--
+//        }
+//    }
+//
+//    fun trimCommaOfString(string: String): String? {
+////        String returnString;
+//        return if (string.contains(",")) {
+//            string.replace(",", "")
+//        } else {
+//            string
+//        }
+//    }
+//}
+
+
+
+//    fun afterTextChanged(s: Editable) {
+//        editText.removeTextChangedListener(this)
+//        try {
+//            var originalString = s.toString()
+//            val longval: Long
+//            if (originalString.contains(",")) {
+//                originalString = originalString.replace(",".toRegex(), "")
+//            }
+//            longval = originalString.toLong()
+//            val formatter = NumberFormat.getInstance(Locale.US) as DecimalFormat
+//            formatter.applyPattern("#,###,###,###")
+//            val formattedString = formatter.format(longval)
+//
+//            //setting text after format to EditText
+//            editText.setText(formattedString)
+//            editText.setSelection(editText.getText().length())
+//        } catch (nfe: NumberFormatException) {
+//            nfe.printStackTrace()
+//        }
+//        editText.addTextChangedListener(this)
+//    }
+
+
+
+//    fun afterTextChanged(view: Editable) {
+//        var s: String? = null
+//        try {
+//            s = String.format("%,d", view.toString().toLong())
+//        } catch (e: NumberFormatException) {
+//        }
+//    }
+
 
 }
--- app/src/main/java/com/example/covid_19/ui/MainActivity.kt
+++ app/src/main/java/com/example/covid_19/ui/MainActivity.kt
@@ -1,30 +1,241 @@
 package com.example.covid_19.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils.isEmpty
+import android.text.method.TextKeyListener.clear
 import android.util.Log
+import android.view.Menu
+import android.view.View
+import android.widget.Adapter
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SearchView
+import androidx.lifecycle.Transformations.distinctUntilChanged
+import androidx.lifecycle.Transformations.map
+import androidx.recyclerview.widget.DefaultItemAnimator
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.covid_19.API.CovidAPI
 import com.example.covid_19.R
-import com.example.covid_19.adapter.Adapter
+import com.example.covid_19.models.Country
+import com.example.covid_19.models.Data
 import com.google.gson.GsonBuilder
 import hu.akarnokd.rxjava3.retrofit.RxJava3CallAdapterFactory
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.internal.schedulers.IoScheduler
 import io.reactivex.rxjava3.schedulers.Schedulers
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.*
 import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
-    var searchBar = findViewById<SearchView>(R.id.search_bar)
+//    private var recyclerView: RecyclerView? = null
+//    private var layoutManager: RecyclerView.LayoutManager? = null
+//    private val data: List<Data> = ArrayList<Data>()
+//    private val adapter: Adapter? = null
+//    private val TAG = MainActivity::class.java.simpleName
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        var searchBar = findViewById<SearchView>(R.id.search_bar)
         var recyclerView: RecyclerView? = null
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        recyclerView = findViewById(R.id.recyclerView)
+//        layoutManager = LinearLayoutManager(this@MainActivity)
+//        recyclerView!!.setLayoutManager(layoutManager)
+//        recyclerView!!.setItemAnimator(DefaultItemAnimator())
+//        recyclerView!!.setNestedScrollingEnabled(false)
+
+        recyclerView = findViewById(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        val url = "https://api.covid19api.com/"
+        val retrofit = Retrofit.Builder().addConverterFactory(
+            GsonConverterFactory.create(
+                GsonBuilder().create()
+            )
+        ).addCallAdapterFactory(RxJava3CallAdapterFactory.create()).baseUrl(url).build()
+
+        val postCountryAPI = retrofit.create(CovidAPI::class.java)
+        val response = postCountryAPI.getAllData()
+        response.observeOn(AndroidSchedulers.mainThread()).subscribeOn(IoScheduler()).subscribe() {
+            recyclerView.adapter = com.example.covid_19.adapter.Adapter(it.Countries, this)
+        }
+
+    }
+}
+
+
+
+//        LoadJSON()
+//    }
+//
+//    class LoadJSON {
+//        val retrofit = Retrofit.Builder()
+//            .baseUrl(MainActivity.Companion.BASE_URL)
+//            .addConverterFactory(GsonConverterFactory.create())
+//            .build()
+//        val request: CovidAPI = retrofit.create(CovidAPI::class.java)
+//        val call: Observable<Country> = request.getAllData()
+//    }
+//
+//    companion object {
+//        const val BASE_URL = "https://api.covid19api.com/"
+//    }
+//
+//    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+//        menuInflater.inflate(R.menu.menu_main, menu)
+//        val searchBar = findViewById<SearchView>(R.id.search_bar)
+//        searchBar = menu?.findItem(R.id.app)
+//
+//
+//        return super.onCreateOptionsMenu(menu)
+//    }
+
+
+//    private fun loadJSON() {
+//        val retrofit = Retrofit.Builder()
+////            .addConverterFactory(GsonConverterFactory.create(GsonBuilder().create()))
+////            .addCallAdapterFactory(RxJava3CallAdapterFactory.create()).baseUrl(url)
+////            .build()
+//            .baseUrl(MainActivity.Companion.BASE_URL)
+//            .addConverterFactory(GsonConverterFactory.create())
+//            .build()
+//        val request: CovidAPI = retrofit.create(CovidAPI::class.java)
+//        val call: Observable<Country> = request.getAllData()
+//    }
+
+
+//    fun search() {
+//        val searchBar = findViewById<SearchView>(R.id.search_bar)
+//        io.reactivex.rxjava3.core.Observable.create<String> { emitter ->
+//            searchBar.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+//                override fun onQueryTextSubmit(query: String?): Boolean {
+//                    return false
+//                }
+//
+//                override fun onQueryTextChange(newText: String?): Boolean {
+//                    if (!emitter.isDisposed) {
+//                        emitter.onNext(newText)
+//                    }
+//                    return false
+//                }
+//            })
+//        }
+//
+//            .debounce(400, TimeUnit.MILLISECONDS)
+//            .filter(text -> !text.isEmpty() && text.length() >= 3)
+//            .map(text -> text.toLowerCase().trim())
+////            .map(CharSequence::toString)
+//            .distinctUntilChanged()
+//            .subscribeOn(Schedulers.io())
+//            .observeOn(AndroidSchedulers.mainThread())
+//            .subscribe {
+//                Log.d("TAG", "Search: $it"
+//                //viewModel.searchCountry(it)
+//
+//
+//
+//            }
+//    },
+//
+//                {
+//                    Log.e("TAG", "Error : $it")
+//                },
+//
+//                {
+//                    Log.d("TAG", "Complete")
+//                }
+//            )
+//    }
+//
+//}
+
+
+
+//        call.enqueue(object : Callback<JSONResponse?>() {
+//            override fun onResponse(call: Call<JSONResponse?>?, response: Response<JSONResponse?>) {
+//                val jsonResponse: JSONResponse? = response.body()
+//                mArrayList = ArrayList<Any?>(Arrays.asList(jsonResponse.getAndroid()))
+//                mAdapter = mArrayList?.let { DataAdapter(it) }
+//                mRecyclerView?.setAdapter(mAdapter)
+//            }
+//
+//            override fun onFailure(call: Call<JSONResponse?>?, t: Throwable) {
+//                Log.d("Error", t.message!!)
+//            }
+//        })
+//    }
+
+
+
+//     private fun LoadJSON() {
+//         var recyclerView: RecyclerView? = null
+//         var layoutManager: RecyclerView.LayoutManager? = null
+//        val url = "https://api.covid19api.com/"
+//        val retrofit = Retrofit.Builder().addConverterFactory(
+//            GsonConverterFactory.create(
+//                GsonBuilder().create()
+//            )
+//        ).addCallAdapterFactory(RxJava3CallAdapterFactory.create()).baseUrl(url).build()
+//
+//        val postCountryAPI = retrofit.create(CovidAPI::class.java)
+//        val response = postCountryAPI.getAllData()
+//        response.observeOn(AndroidSchedulers.mainThread()).subscribeOn(IoScheduler()).subscribe() {
+//            recyclerView!!.adapter = Adapter(it.Countries, this)
+//        }
+
+//        String country = Utils.getCountry();
+//
+//        Call<CovidAPI> call;
+//        Call = CovidAPI.getAllData(Country, country);
+//        Call.enqueue(new Call.Callback<Covid>(){
+
+
+//            @Override
+//            public void onResponse(Call<CovidAPI> call, Response<Country> response) {
+//                    if (!Country.isEmpty()) {
+//                        Country.clear();
+//                    }
+//
+//                    country = response.body().getCountry();
+//                    adapter = new Adapter(country, MainActivity.this);
+//                    recyclerView?.setAdapter();
+//                    adapter.notifyDataSetChanged();
+//            }
+//            else {
+//                             Toast.makeText(MainActivity.this,"No Result!", Toast.LENGTH_SHORT).show();
+//                         }
+//
+//
+//                     @Override
+//                             public void onFailure(Call<Covid> call, Throwable t) {
+//
+//        }
+
+
+//    });
+//}
+//
+//
+//
+//
+//
+//}
+
+//
+//        var recyclerView: RecyclerView? = null
+//        val searchBar = findViewById<SearchView>(R.id.search_bar)
+
+
+
+
+//        search()
 
 //        recyclerView = findViewById(R.id.recyclerView)
 //        recyclerView.layoutManager = LinearLayoutManager(this)
@@ -38,62 +249,58 @@ class MainActivity : AppCompatActivity() {
 //        val postCountryAPI = retrofit.create(CovidAPI::class.java)
 //        val response = postCountryAPI.getAllData()
 //        response.observeOn(AndroidSchedulers.mainThread()).subscribeOn(IoScheduler()).subscribe() {
-//            recyclerView.adapter = Adapter(it.Countries, this)
+//            recyclerView!!.adapter = Adapter(it.Countries, this)
+//        }
+
+
+
+
+//RxTextView.textChangeEvents(inputSearch)
+
+//.map(new Func1<TextViewTextChangeEvent, String>() {
+//    @Override
+//    public String call(TextViewTextChangeEvent textViewTextChangeEvent) {
+//        return textViewTextChangeEvent.text().toString()
+//    }
+//})
+//.switchMap(new Func1<String, Observable<List<Result>>>() {
+//    @Override
+//    public Observable < List < Result > > call(String s) {
+//        DiscoverAPI discoverAPI = restAdapter . create DiscoverAPI.class)
+//        return discoverAPI.getRxSearchResult(0, 9, s)
+//    }
+//})
+//.observeOn(AndroidSchedulers.mainThread())
+//.subscribe(getRxSearchObserver())
+//
+//
+//private Observer<List<Data>> getRxSearchObserver() {
+//    return new Observer < List < Result > > () {
+//        @Override
+//        public void onCompleted() {
+//            LogUtil.i(TAG, "onCompleted called")
 //        }
 //
+//        @Override
+//        public void onError(Throwable e) {
+//            LogUtil.i(TAG, "onError called")
+//        }
+//
+//        @Override
+//        public void onNext(List<Data> results) {
+//            LogUtil.i(TAG, "onNext called")
+//            list.clear()
+//            if (adapter != null) {
+//                adapter.notifyDataSetInvalidated()
+//            }
+//            list.addAll(results)
+//            if (adapter == null) {
+//                adapter = new SearchGalleryAdapter TestActivity.this, list)
+//                listView.setAdapter(adapter)
+//            } else {
+//                adapter.notifyDataSetChanged()
+//            }
+//            indicator.setVisibility(View.GONE)
+//        }
 //    }
-
-        fun search() {
-            io.reactivex.rxjava3.core.Observable.create<String> { emitter ->
-                searchBar.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
-                    override fun onQueryTextSubmit(query: String?): Boolean {
-                        return false
-                    }
-
-                    override fun onQueryTextChange(newText: String?): Boolean {
-                        if (!emitter.isDisposed) {
-                            emitter.onNext(newText)
-                        }
-                        return false
-                    }
-                })
-            }
-
-                .debounce(500, TimeUnit.MILLISECONDS)
-                .distinctUntilChanged()
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(
-                    {
-                        Log.d("TAG", "Search: $it")
-                        //viewModel.searchCountry(it)
-                        var recyclerView: RecyclerView? = null
-                        recyclerView = findViewById(R.id.recyclerView)
-                        recyclerView.layoutManager = LinearLayoutManager(this)
-                        val url = "https://api.covid19api.com/"
-                        val retrofit = Retrofit.Builder().addConverterFactory(
-                            GsonConverterFactory.create(
-                                GsonBuilder().create()
-                            )
-                        ).addCallAdapterFactory(RxJava3CallAdapterFactory.create()).baseUrl(url)
-                            .build()
-
-                        val postCountryAPI = retrofit.create(CovidAPI::class.java)
-                        val response = postCountryAPI.getAllData()
-                        response.observeOn(AndroidSchedulers.mainThread())
-                            .subscribeOn(IoScheduler()).subscribe() {
-                            recyclerView.adapter = Adapter(it.Countries, this)
-                        }
-                    },
-
-                    {
-                        Log.e("TAG", "Error : $it")
-                    },
-
-                    {
-                        Log.d("TAG", "Complete")
-                    }
-                )
-        }
-    }
-}
+//}
